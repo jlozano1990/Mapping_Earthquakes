57cde26cd2597201396b53c37f8fe0104a712578
jlozano1990 <jlozano1990@gmail.com>
2021-03-02 23:00:23 -0500

    Fixing

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -6,7 +6,9 @@ let map = L.map('mapid').setView([40.7, -94.5], 14);
 
 //  Add a marker to the map for Los Angeles, California.
 L.circle([34.0522, -118.2437], {
-    radius: 100
+    radius: 300,
+    color:"black",
+    fillColor:"ffffa1"
  }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
