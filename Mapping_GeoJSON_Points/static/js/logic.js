57233fce713be2cc9c16621542342e476f299ac8
jlozano1990 <jlozano1990@gmail.com>
2021-03-06 18:08:33 -0500

    Adding Airports.json

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -26,17 +26,18 @@ let sanFranAirport =
 
 // Grabbing our GeoJSON data.
 L.geoJson(sanFranAirport, {
+  // We turn each feature into a marker on the map.
   onEachFeature: function(feature, layer) {
     console.log(layer);
-    layer.bindPopup("<h2>Airport Code: " + feature.properties.faa + "</h2><h3>Airport Name: " + feature.properties.name + "</h3>");
-    }
-  }).addTo(map);
+    layer.bindPopup("<h2>Airport Code: " + feature.properties.faa + "</h2><hr></h3>Airport Name: " + feature.properties.name + "</h3>");
+  }
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: "mapbox/satellite-streets-v11",
+    id: "mapbox/streets-v11",
     accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
